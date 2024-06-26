eee23ac4ab213f7e43d7faf41acd098170585e12
Kyungbin Min <kbmin2711@gmail.com>
2024-06-26 14:38:42 +0900

    Update configuration

--- FlightComputer/config.h
+++ FlightComputer/config.h
@@ -9,7 +9,9 @@
 /*-------------------------------------------------------------------*/
 /* Define or modify the configurations here if you want. */
 
-#define SERVO_PIN 9        // Servo motor pin number
+#define MAX_DATA_COUNT 10  // Maximum number of sensor output data.
+#define IMU_DATA_COUNT 3   // Number of BNO055 output data.
+#define SERVO_PIN 2        // Servo motor pin number.
 #define SAMPLING_RATE 100  // Sampling rate, in Hz.
 #define ROTATION_SPEED 0.2 // Rotation speed in (degree/ms).
 
