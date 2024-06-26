6043bc1dda85e10766147e2abbe2aadf1f9aa318
Kyungbin Min <kbmin2711@gmail.com>
2024-06-26 21:04:05 +0900

    Update FlightComputer code to include non-blocking delay

--- FlightComputer/FlightComputer.ino
+++ FlightComputer/FlightComputer.ino
@@ -14,10 +14,15 @@ static NonBlockingServo serv;
 static IMUSensor imu_sensor;
 static GPSSensor gps_sensor;
 
+/* Time variables for non-blocking delay(). */
+static unsigned long last_update_time = 0;
+static unsigned long current_time;
+
 void setup() {
   Serial.begin(BAUD_RATE);
   Serial.println("Hello World!"); // serial monitor test.
   serv.begin();
+  imu_sensor.begin();
   gps_sensor.begin();
 }
 
@@ -25,5 +30,17 @@ void loop() {
   /*-----------------------------------------------------------------*/
   /* Servo part. */
   updateServoFromSerial(serv);
+  /*-----------------------------------------------------------------*/
+
+  /*-----------------------------------------------------------------*/
+  /* Sensor update part. */
+  current_time = millis();
+  if (current_time - last_update_time > (1000 / SAMPLING_RATE)) {
+    last_update_time = current_time;
+    imu_sensor.update();
+    gps_sensor.update();
+    printSensorDataToSerial(imu_sensor);
+    printSensorDataToSerial(gps_sensor);
+  }
   /*-----------------------------------------------------------------*/
 }
