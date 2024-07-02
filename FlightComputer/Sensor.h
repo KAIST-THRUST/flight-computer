d96fa8294fd5fada360d0d0e1a499b339fc69510
Kyungbin Min <kbmin2711@gmail.com>
2024-07-03 00:14:33 +0900

    Add AverageSensorValue class to Sensor.h

--- FlightComputer/Sensor.h
+++ FlightComputer/Sensor.h
@@ -15,9 +15,9 @@ struct SensorData {
 
 class Sensor {
 public:
-  virtual void begin() = 0;      // Begin method. Place it in setup().
-  virtual void update() = 0;     // Update the sensorData attribute.
-  virtual ~Sensor() = default;   // Default destructor.
+  virtual void begin() = 0;    // Begin method. Place it in setup().
+  virtual void update() = 0;   // Update the sensorData attribute.
+  virtual ~Sensor() = default; // Default destructor.
   virtual String toString() const = 0; // Convert sensor data to string.
 
   const SensorData &getSensorData() const { // Getter for sensorData.
@@ -26,6 +26,28 @@ public:
 
 protected:
   SensorData sensorData; // Attribute holding sensor readings.
+};
+
+class AverageSensorValue {
+public:
+  AverageSensorValue() : avg(0.0), count(0) {}
+
+  void addValue(float value) {
+    /* Calculate the new average. */
+    avg = avg * (count / (count + 1.0)) + value / (count + 1.0);
+    count++;
+  }
+
+  float getAverage() const { return avg; }
+
+  void reset() {
+    avg = 0;
+    count = 0;
+  }
+
+private:
+  float avg;
+  int count;
 };
 
 #endif
