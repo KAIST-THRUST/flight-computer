5fc5f91d59f86a1208e4a4cb0ec1e19963396a13
Kyungbin Min <kbmin2711@gmail.com>
2024-07-03 12:23:21 +0900

    Add getter in SensorSet class and modify addSensor method

--- FlightComputer/SensorSet.h
+++ FlightComputer/SensorSet.h
@@ -6,7 +6,12 @@
 #ifndef SENSORSET_H
 #define SENSORSET_H
 
+#include "BarometerSensor.h"
+#include "GPSSensor.h"
+#include "IMUSensor.h"
 #include "Sensor.h"
+#include "VoltmeterSensor.h"
+#include "config.h"
 
 class SensorSet {
 public:
@@ -19,10 +24,21 @@ public:
     SENSOR_COUNT
   };
   SensorSet();
-  // Add a sensor to the set.
+  /* Add a sensor to the set. */
   void addSensor(Sensor *sensor, enum SensorType sensor_type);
+  void addSensor(GPSSensor *sensor);
+  void addSensor(IMUSensor *sensor);
+  void addSensor(BarometerSensor *sensor);
+  void addSensor(VoltmeterSensor *sensor);
+
   void beginAll();  // Begin all sensors.
   void updateAll(); // Update all sensors.
+
+  /* Getter for specific sensor data. */
+  float getValue(enum GPSSensor::GPSDataIndex index) const;
+  float getValue(enum IMUSensor::IMUDataIndex index) const;
+  float getValue(enum BarometerSensor::BarometerDataIndex index) const;
+  float getValue(enum VoltmeterSensor::VoltmeterDataIndex index) const;
 
 private:
   Sensor *sensors[SENSOR_COUNT];
--- FlightComputer/SensorSet.ino
+++ FlightComputer/SensorSet.ino
@@ -6,14 +6,46 @@ void SensorSet::addSensor(Sensor *sensor, enum SensorType sensor_type) {
   sensors[sensor_type] = sensor;
 }
 
+void SensorSet::addSensor(GPSSensor *sensor) { sensors[GPS] = sensor; }
+
+void SensorSet::addSensor(IMUSensor *sensor) { sensors[IMU] = sensor; }
+
+void SensorSet::addSensor(BarometerSensor *sensor) {
+  sensors[BAROMETER] = sensor;
+}
+
+void SensorSet::addSensor(VoltmeterSensor *sensor) {
+  sensors[VOLTMETER] = sensor;
+}
+
 void SensorSet::beginAll() {
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    sensors[i]->begin();
+  /* Begin all sensors. */
+  for (Sensor *sensor : sensors) {
+    sensor->begin();
   }
 }
 
 void SensorSet::updateAll() {
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    sensors[i]->update();
+  /* Update all sensors. */
+  for (Sensor *sensor : sensors) {
+    sensor->update();
   }
+}
+
+float SensorSet::getValue(enum GPSSensor::GPSDataIndex index) const {
+  return sensors[GPS]->getSensorData().values[index];
+}
+
+float SensorSet::getValue(enum IMUSensor::IMUDataIndex index) const {
+  return sensors[IMU]->getSensorData().values[index];
+}
+
+float SensorSet::getValue(
+    enum BarometerSensor::BarometerDataIndex index) const {
+  return sensors[BAROMETER]->getSensorData().values[index];
+}
+
+float SensorSet::getValue(
+    enum VoltmeterSensor::VoltmeterDataIndex index) const {
+  return sensors[VOLTMETER]->getSensorData().values[index];
 }
