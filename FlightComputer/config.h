63869781baf66a9925cb7825527998bf14c3bd2f
Kyungbin Min <kbmin2711@gmail.com>
2024-07-08 21:34:48 +0900

    Fix typo in config.h

--- FlightComputer/config.h
+++ FlightComputer/config.h
@@ -21,11 +21,11 @@
 /* Barometer sensor(BMP280) configurations. */
 #define BAROMETER_I2C_ADDRESS 0x76 // I2C address of BMP280 sensor.
 #define BAROMETER_WIRE Wire1       // Wire name connected to BMP280.
+#define SEA_LEVEL_HPA 1013.25      // Sea level pressure in hPa.
 
 /* IMU sensor(BNO055) configurations. */
-#define IMU_I2C_ADDRESS 0x28  // I2C address of BNO055 sensor.
-#define IMU_WIRE Wire         // Wire name connected to BNO055.
-#define SEA_LEVEL_HPA 1013.25 // Sea level pressure in hPa.
+#define IMU_I2C_ADDRESS 0x28 // I2C address of BNO055 sensor.
+#define IMU_WIRE Wire        // Wire name connected to BNO055.
 
 /* GPS sensor configurations. */
 #define GPS_SERIAL Serial2  // Serial name connected to GPS.
