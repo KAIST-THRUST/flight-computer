cf6152f0e01aa09afb53399969c73c602ebc1de4
Kyungbin Min <kbmin2711@gmail.com>
2024-07-16 15:01:50 +0900

    Implement basic state machine structure and coast state

--- FlightComputer/FlightComputer.ino
+++ FlightComputer/FlightComputer.ino
@@ -22,8 +22,8 @@ static ADCSensor adc_sensor;
 static SDManager sd_manager;
 
 /* Time variables for non-blocking delay. */
-static unsigned long last_update_time = 0;
-static unsigned long current_time;
+static uint32_t last_update_time = 0;
+static uint32_t current_time;
 static RealTimeClock rtc;
 
 void setup() {
@@ -41,52 +41,56 @@ void setup() {
 }
 
 void loop() {
-  // switch (rocket_current_state) {
-  // case RocketState::ST_STAND_BY:
-  //   // Stand by RocketState.
-  //   break;
-
-  // case RocketState::ST_BURN:
-  //   // Burn RocketState.
-  //   break;
-
-  // case RocketState::ST_COAST:
-  //   // Coast RocketState.
-  //   break;
-
-  // case RocketState::ST_DESCENT:
-  //   // Descent RocketState.
-  //   break;
-
-  // case RocketState::ST_LANDED:
-  //   // Landed RocketState.
-  //   break;
-  // }
-
   /*-----------------------------------------------------------------*/
   /* Servo part. */
   updateServoFromSerial(serv);
   /*-----------------------------------------------------------------*/
 
-  /*-----------------------------------------------------------------*/
-  /* Sensor update part. */
-  current_time = millis();
-  gps_sensor.update();
+  switch (rocket_current_state) {
+  case RocketState::ST_STAND_BY:
+    /* Stand by state. */
+    break;
 
-  if (current_time - last_update_time >= (1000 / SAMPLING_RATE)) {
-    last_update_time = current_time;
-    imu_sensor.update();
-    barometer_sensor.update();
-    adc_sensor.update();
-    Serial.println("------------------------------------------------");
-    Serial.print("Time: ");
-    Serial.println(current_time);
-    Serial.println(rtc.getTimeData());
-    printSensorDataToSerial(imu_sensor);
-    printSensorDataToSerial(gps_sensor);
-    printSensorDataToSerial(barometer_sensor);
-    printSensorDataToSerial(adc_sensor);
-    Serial.println("------------------------------------------------");
+  case RocketState::ST_BURN:
+    /* Burn state. */
+    break;
+
+  case RocketState::ST_COAST:
+    /* Coast state. */
+    current_time = millis();
+    gps_sensor.update(); // Need to be updated every loop.
+    if (current_time - last_update_time >= (1000 / SAMPLING_RATE)) {
+      last_update_time = current_time;
+      imu_sensor.update();
+      barometer_sensor.update();
+      adc_sensor.update();
+
+      /* Logging sensor data to Serial. */
+      Serial.println("----------------------------------------------");
+      Serial.print("Time: ");
+      Serial.println(current_time);
+      Serial.println(rtc.getTimeData());
+      printSensorDataToSerial(imu_sensor);
+      printSensorDataToSerial(gps_sensor);
+      printSensorDataToSerial(barometer_sensor);
+      printSensorDataToSerial(adc_sensor);
+      Serial.println("----------------------------------------------");
+
+      /* Writing sensor data to SD card. */
+      sd_manager.write(rtc.getTimeData());
+      sd_manager.write(imu_sensor.toString());
+      sd_manager.write(gps_sensor.toString());
+      sd_manager.write(barometer_sensor.toString());
+      sd_manager.write(adc_sensor.toString());
+    }
+    break;
+
+  case RocketState::ST_DESCENT:
+    /* Descent state. */
+    break;
+
+  case RocketState::ST_LANDED:
+    /* Landed state. */
+    break;
   }
-  /*-----------------------------------------------------------------*/
 }
